5229a514ee8d588d752e749cdb6254170b439d65
maaz <mizerablepi@gmail.com>
2024-01-31 21:21:36 +0530

    create root component

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,9 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import NavaBar from "./Components/Navbar";
+import { useEffect, useState } from "react";
+import Footer from "./Components/Footer";
 
 function Root() {
+  const [username, setUsername] = useState(null);
+  const url = useLocation();
+
+  useEffect(() => {
+    const user = window.localStorage.getItem("username");
+    setUsername(user);
+  }, [url]);
   return (
     <>
-      <Outlet></Outlet>
+      <NavaBar username={username} setUsername={setUsername} />
+      <Outlet />
+      <Footer />
     </>
   );
 }
