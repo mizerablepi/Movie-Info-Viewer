e01bf496e295d2de6f169a1d04b2b46e6a1ae1bf
maaz <mizerablepi@gmail.com>
2024-01-31 21:31:14 +0530

    update styles

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -56,7 +56,7 @@ function Login() {
         />
         <button
           type="submit"
-          className="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none self-start"
+          className="select-none rounded-lg bg-pink-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] active:shadow-none self-start"
         >
           Login
         </button>
