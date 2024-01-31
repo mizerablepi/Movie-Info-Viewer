33038f6089b6e03c6e924b9008d3e390bcc69827
maaz <mizerablepi@gmail.com>
2024-01-31 23:06:10 +0530

    complete show list page

--- src/Pages/ShowList.jsx
+++ src/Pages/ShowList.jsx
@@ -1,5 +1,28 @@
+import { useState, useEffect } from "react";
+import Card from "../Components/Card";
+
 function ShowList() {
-  return <>ALL SHOWS</>;
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch("https://api.tvmaze.com/search/shows?q=all")
+      .then((res) => res.json())
+      .then((output) => setData(output));
+  }, []);
+
+  console.log(data);
+
+  // console.log(data[0].show);
+  return data ? (
+    <>
+      <div className="grid grid-cols-3 gap-y-16 my-10">
+        {data.map((show) => {
+          return <Card show={show.show} key={show.show.id} />;
+        })}
+      </div>
+    </>
+  ) : (
+    <>Loading...</>
+  );
 }
 
 export default ShowList;
