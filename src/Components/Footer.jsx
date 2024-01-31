2a7f45bb9e8741ac059be35589f9885b3ca6eb54
maaz <mizerablepi@gmail.com>
2024-01-31 21:03:16 +0530

    add footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer className="bg-red-500 p-4 mt-auto">
+      <div className="text-center font-bold text-white">
+        built by{" "}
+        <a className="underline" href="https://www.github.com/mizerablepi">
+          mizerable
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
