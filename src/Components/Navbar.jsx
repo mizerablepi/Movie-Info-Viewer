3dc78dbf01e2a4b1602cd8ef365d9a71154dfa4c
maaz <mizerablepi@gmail.com>
2024-01-31 20:52:49 +0530

    add navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+function NavaBar({ username, setUsername }) {
+  const url = useLocation();
+
+  const logOut = () => {
+    window.localStorage.removeItem("username");
+    window.localStorage.removeItem("email");
+    window.localStorage.removeItem("password");
+    setUsername(null);
+  };
+
+  return (
+    <nav className="flex gap-4 bg-red-500 p-4 ">
+      <div className="flex items-center">
+        <span className="font-bold text-2xl text-white font-serif italic">
+          SeeMyShow
+        </span>
+      </div>
+      <div className="flex items-center mx-auto">
+        <Link
+          to={"/"}
+          className={`text-lg font-bold text-white px-4 py-2  hover:bg-red-600 hover:shadow transition rounded decoration-4 underline-offset-8 ${
+            url.pathname === "/" ? "underline" : ""
+          }`}
+        >
+          Home
+        </Link>
+        <Link
+          to={"/shows"}
+          className={`text-lg font-bold text-white px-4 py-2 hover:bg-red-600 hover:shadow transition rounded decoration-4 underline-offset-8 ${
+            url.pathname === "/shows" ? "underline" : ""
+          }`}
+        >
+          Shows
+        </Link>
+      </div>
+      {username ? (
+        <button
+          onClick={logOut}
+          className="bg-white font-bold text-red-600 px-4 py-2 rounded-md"
+        >
+          {`HI, ${username}`}
+        </button>
+      ) : (
+        <Link
+          to={"/login"}
+          className="bg-white font-bold text-red-600 px-4 py-2 rounded-md"
+        >
+          Login
+        </Link>
+      )}
+    </nav>
+  );
+}
+
+export default NavaBar;
